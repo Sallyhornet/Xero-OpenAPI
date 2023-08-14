4efdd096eaf99425f632d02c028643a291551c2b
Brian <brian.kim@xero.com>
2023-08-14 18:17:24 +1000

    Addition of dotnet replacing netcore/standard

--- generator/scripts/csharp/dotnet-oauth2-accounting.sh
+++ generator/scripts/csharp/dotnet-oauth2-accounting.sh
@@ -8,25 +8,24 @@ echo "# START SCRIPT: $SCRIPT"
 
 # remote yaml on github branch "oauth"
 ags="generate 
--t ./generator/modules/csharp-netcore 
+-t ./generator/modules/csharp-dotnet
 -i https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/master/accounting-yaml/Xero_accounting_2.0.0_swagger.yaml 
--g csharp-netcore
--o ./generator/output/csharp-netcore/accounting
+-g csharp-dotnet
+-o ./generator/output/csharp-dotnet/accounting
 -c ./generator/scripts/dotnet-oauth2-accounting.json
 -p debugModels=false
 -p hideGenerationTimestamp=true
 $@"
 
-echo "Removing files and folders under output/output/csharp-netcore"
-rm -rf ./generator/output/csharp-netcore/accounting
+echo "Removing files and folders under output/output/csharp-dotnet"
+rm -rf ./generator/output/csharp-dotnet/accounting
 openapi-generator $ags  
 # hacky way of fixing some things without editing the templating engine
-rm -rf generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/project.json
+rm -rf generator/output/csharp-dotnet/accounting/src/Xero.DotNet.OAuth2/project.json
 # following to make it compatible with VS2019
-sed -e '/<TargetFrameworkIdentifier>.NETStandard<\/TargetFrameworkIdentifier>/d' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Xero.NetStandard.OAuth2.csproj
-sed -e 's/<TargetFrameworkVersion>v2.0<\/TargetFrameworkVersion>/<TargetFramework>netstandard2.0<\/TargetFramework>/g' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Xero.NetStandard.OAuth2.csproj
+sed -e 's/<TargetFramework>net6.0<\/TargetFramework>/g' -i generator/output/csharp-dotnet/accounting/src/Xero.DotNet.OAuth2/Xero.DotNet.OAuth2.csproj
 # fix emitdefaultvalue for models
-find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/string? /string /g' -i
-find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/DateTime?? /DateTime? /g' -i
-find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/Guid?? /Guid? /g' -i
-find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/List<string>? /List<string> /g' -i
+find generator/output/csharp-dotnet/accounting/src/Xero.DotNet.OAuth2/Model/*.cs -type f | xargs sed -e 's/string? /string /g' -i
+find generator/output/csharp-dotnet/accounting/src/Xero.DotNet.OAuth2/Model/*.cs -type f | xargs sed -e 's/DateTime?? /DateTime? /g' -i
+find generator/output/csharp-dotnet/accounting/src/Xero.DotNet.OAuth2/Model/*.cs -type f | xargs sed -e 's/Guid?? /Guid? /g' -i
+find generator/output/csharp-dotnet/accounting/src/Xero.DotNet.OAuth2/Model/*.cs -type f | xargs sed -e 's/List<string>? /List<string> /g' -i
